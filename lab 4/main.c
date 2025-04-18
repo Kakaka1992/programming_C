912f8355246b8e0396ef42efed0fc761d9b5756a
Alina Sashina <164209203+Kakaka1992@users.noreply.github.com>
2025-04-18 23:11:41 +1000

    Update main.c

--- lab 4/main.c
+++ lab 4/main.c
@@ -6,8 +6,13 @@ int main() {
     printf("vvedite 3 znacheniya: ");
     scanf("%lf %lf %lf", &a, &b, &c);
 
+    double p = perimeter(a, b, c);
+    double s = area(a, b, c);
+    double h = height(a, s);
+
     printf("perimitr: %.2lf\n", perimeter(a, b, c));
     printf("ploshad: %.2lf\n", area(a, b, c));
+    printf("Высота к стороне a: %.2lf\n", h);
 
     return 0;
 }
