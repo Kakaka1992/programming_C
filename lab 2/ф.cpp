337c6224df1262bdc52a75d3042cbe87dac11333
Alina Sashina <164209203+Kakaka1992@users.noreply.github.com>
2025-05-02 21:59:51 +1000

    Update ф.cpp

--- lab 2/ф.cpp
+++ lab 2/ф.cpp
@@ -1,37 +1,54 @@
 #include <stdio.h>
 
 int main() {
-    double a[3][3];
-    printf("vvesti 9 chisel 3x3:\n");
-    for(int i = 0; i < 3; i++) 
-        for(int j = 0; j < 3; j++) 
-            scanf("%lf", &a[i][j]);
-    
-    double main_sum = 0, anti_sum = 0;
-    
-    for(int i = 0; i < 3; i++) {
-        main_sum += a[i][i];
-    }
-    
-    for(int i = 0; i < 3; i++) {
-        anti_sum += a[i][2 - i];
+    double m[3][3]; 
+    double gdiag = 0, pdiag = 0;
+    int i, j;
+
+   
+    printf("9 chisel dlya 3x3:\n");
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            scanf("%lf", &m[i][j]);
+        }
     }
 
-    int b[2][2], res[2][2] = {0};
-    printf("vvesti 4 chisla 2x2:\n");
-    for(int i = 0; i < 2; i++)
-        for(int j = 0; j < 2; j++)
-            scanf("%d", &b[i][j]);
+   
+    for (i = 0; i < 3; i++) {
+        gdiag += m[i][i];
+        pdiag += m[i][2 - i];
+    }
+
+    printf("glavnaya: %.1lf\n", gdiag);
+    printf("pobochnaya: %.1lf\n", pdiag);
+
+	
+	{
+    int m[2][2];
     
-    for(int i = 0; i < 2; i++)
-        for(int j = 0; j < 2; j++)
-            for(int k = 0; k < 2; k++)
-                res[i][j] += b[i][k] * b[k][j];
     
-    printf("kvadrat:\n");
-    for(int i = 0; i < 2; i++) {
-        printf("%d %d\n", res[i][0], res[i][1]);
+    printf("4 chisla dlya 2x2:\n");
+    int i, j, k;
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            scanf("%d", &m[i][j]);
+        }
+    }
+
+    
+    printf("\nkvadrat:\n");
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            int sum = 0;
+            
+            for(k = 0; k < 2; k++) {
+                sum += m[i][k] * m[k][j];
+            }
+            printf("%d ", sum);
+        }
+        printf("\n");
     }
 
     return 0;
+}
 }
